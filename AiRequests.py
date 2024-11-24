550b90114a0aa6fa83412efe385267e3e371f6ee
Ostap Franchuk <ostapf63@gmail.com>
2024-11-25 00:32:15 +0100

    all my homies love pacner

--- AiRequests.py
+++ AiRequests.py
@@ -12,14 +12,15 @@ aiModel = "llama3-8b-8192"
 
 userMessage = "TestMessage"
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": f"{userMessage}",
-        }
-    ],
-    model=aiModel,
-)
-
-print(chat_completion.choices[0].message.content)
+def AskGpt(userMessage, aiModel):                               #method to use in bot to pass the message from the user and return the answer from an AI model
+    chat_completion = client.chat.completions.create(
+        messages=[
+                {
+                "role": "user",
+                "content": f"{userMessage}",
+                }
+            ],
+        model=aiModel,
+        )
+    replyMessage = chat_completion.choices[0].message.content
+    return replyMessage
